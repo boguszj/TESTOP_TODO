7739ce102b12d7e1db299c962414a5c3809b3809
karolina <karolina.czachorska@gmail.com>
2020-10-25 12:02:48 +0100

    test context

---
+++ src/test/java/com/tesop/testop/TestopApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tesop.testop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootTest
+class TestopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
